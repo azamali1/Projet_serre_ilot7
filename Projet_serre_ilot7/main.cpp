0a6e6ec94cfce66edc420afe49b1e934e0d411d0
azamali <azamali@btslivh.eu>
2018-12-20 15:04:26 +0100

    Test1

--- Projet_serre_ilot7/main.cpp
+++ Projet_serre_ilot7/main.cpp
@@ -1,12 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
  * File:   main.cpp
- * Author: snir2g2
+ * Author: ilot7 NRZ
  *
  * Created on 20 décembre 2018, 14:43
  */
